259b7ae615407d495d0661ef31bd969f83ce0b55
Szabó Barbara <szabobarbi1996@gmail.com>
2026-06-15 23:22:38 +0200

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -7,6 +7,10 @@ class HealthResponse(BaseModel):
     message: str
 
 
+class PredictByCaseRequest(BaseModel):
+    case_id: str
+
+
 class PredictionResult(BaseModel):
     tumor_detected: bool
     tumor_voxel_count: int
@@ -14,11 +18,12 @@ class PredictionResult(BaseModel):
     max_probability: float
     input_shape: List[int]
     cropped_shape: List[int]
+    used_files: Dict[str, str]
     note: Optional[str] = None
 
 
 class PredictResponse(BaseModel):
     status: str
     message: str
-    received_files: Dict[str, str]
+    case_id: str
     result: PredictionResult
